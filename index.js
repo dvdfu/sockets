f6d6378b4bc9543c95de320bb056dee3c702e59e
dvdfu <davidf1212@gmail.com>
2015-02-06 08:35:27 -0500

    css update

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static(__dirname+'/public'));
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
 	console.log('a user connected');
-	socket.on('disconnect', function(){
+	socket.on('disconnect', function() {
 		console.log('user disconnected');
 	});
-	socket.on('chat message', function(msg){
+	socket.on('chat message', function(msg) {
 		console.log('message: ' + msg);
 		io.emit('chat message', msg);
 	});
 });
 
-http.listen(process.env.PORT || 3000, function(){
+http.listen(process.env.PORT || 3000, function() {
 	console.log('listening on *:3000');
 });
